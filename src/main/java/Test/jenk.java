22d8af17e260819e819d84b777232b6009b24ec6
SunilGarkoti93 <sunil.garkoti@myupchar.com>
2020-08-12 19:00:08 +0530

    First

---
+++ src/main/java/Test/jenk.java
@@ -0,0 +1,22 @@
+package Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class jenk {
+
+
+   @Test
+   public void api_Hit() {
+       Assert.assertTrue(false, "chanhes");
+   }
+
+   @Test
+   public void Test2() {
+
+
+       System.out.println("Test2 execute");
+   }
+
+
+}
